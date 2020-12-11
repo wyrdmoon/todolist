78553cd334a131fd014b18b96dbfb15daafb1dc1
ccorcoran <30421916+wyrdmoon@users.noreply.github.com>
2020-12-10 21:26:18 -0700

    pushing

---
+++ todolist/src/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="Login">
+    <p>EMAIL</p>
+    <input type="text" id="email-input" v-model="email" />
+    <p>PASSWORD</p>
+    <input type="password" id="password-input" v-model="password" />
+    <h2 @click="loginUser">Login</h2>
+    <h3>{{ loginStatus }}</h3>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import cookies from "vue-cookies";
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.Login {
+  width: 500px;
+  margin: auto;
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  grid-template-rows: repeat(auto-fit, minmax, (250px, 1fr));
+  background-color: white;
+  font: FixedSys;
+}
+</style>
---
+++ todolist/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import cookies from 'vue-cookies'
+import Login from './components/Login.vue'
+import SignUp from './components/SignUp.vue'
+
+export default {
+  name: "Home",
+  components: {
+        Login,
+        SignUp,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
